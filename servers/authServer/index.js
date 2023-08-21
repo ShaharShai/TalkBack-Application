4a476377f5b8dbe0f47a00680a4f174cc433fe72
Shahar <shahars839@gmail.com>
2023-08-21 12:31:19 +0300

    Added Routes

--- servers/authServer/index.js
+++ servers/authServer/index.js
@@ -10,6 +10,7 @@ const Account = require('./models/Account');
 const http = require('http');
 const io = require('socket.io')
 
+
 const dbUrl = "mongodb://127.0.0.1:27017/TalkBack"
 
 const app = express();
@@ -17,22 +18,37 @@ const httpServer = http.createServer(app);
 
 const ioServer = io(httpServer, {
     cors: {
-      origin: "http://localhost:5175",
+      origin: "http://localhost:5173",
     },
 })
 
 app.use(express.json());
 app.use(cors());
 
-const users = [
-  {
-    username: "Kyle",
-    password: "1234",
-  },
-];
+const accountRoute = require("./routes/accountRoute")
+app.use("/users", accountRoute)
+
+const activeUsers = {};
 
 ioServer.on("connection", (socket) => {
     console.log(`user ${socket.id} connected`);
+
+    const sockets = ioServer.fetchSockets();
+    
+    socket.on("userConnected", (username) => {
+      activeUsers[socket.id] = username;
+      console.log(`${username} is now online`);
+      ioServer.emit("updateActiveUsers", Object.values(activeUsers)); 
+  });
+
+  socket.on("disconnect", () => {
+    const username = activeUsers[socket.id];
+    if (username) {
+        console.log(`${username} disconnected`);
+        delete activeUsers[socket.id];
+        ioServer.emit("updateActiveUsers", Object.values(activeUsers));
+    }
+});
 
     socket.on("sendMessage", (message) => {
         console.log(message)
@@ -57,46 +73,49 @@ const authenticateToken = async (req, res, next) => {
     
     }
 
-app.get("/users", authenticateToken, async (req, res) => {
-  const users = await Account.find()
-  res.send(users)
+// app.get("/users", authenticateToken, async (req, res) => {
+//   const users = await Account.find()
+//   res.send(users)
   
-});
-
-app.post("/users", async (req, res) => {
-    bcrypt.hash(req.body.password, 10).then(async (hashedPassword) => {
-    const account = new Account({ username: req.body.username, password: hashedPassword });
-    try{
-        await account.save();
-        res.send(account)
-    }
-     catch(err) {
-    res.status(500);
-    res.send(err);
-  }
-});
-})
+// });
 
 
-app.post("/users/login", async (req, res) => {
-  const account = await Account.findOne({username: req.body.username});
-  if (account) {
-    bcrypt.compare(req.body.password, account.password).then((result) => {
-        if(result) {
-           const token = jwt.sign(account.username, process.env.ACCESS_TOKEN_SECRET)
-            res.send({ accessToken: token, username: account.username });
-        }else {
-            res.status(400);
-            res.send("Inccorect Username or Password !");
-            res.end();
-          }
-    })
-  }else{
-    res.status(404);
-    res.send("User Does not Exist !");
-    res.end();
-  }
-});
+
+// app.post("/users", async (req, res) => {
+//     bcrypt.hash(req.body.password, 10).then(async (hashedPassword) => {
+//     const account = new Account({ username: req.body.username, password: hashedPassword });
+//     try{
+//         await account.save();
+//         res.send(account)
+//     }
+//      catch(err) {
+//     res.status(500);
+//     res.send(err);
+//   }
+// });
+// })
+
+
+
+// app.post("/users/login", async (req, res) => {
+//   const account = await Account.findOne({username: req.body.username});
+//   if (account) {
+//     bcrypt.compare(req.body.password, account.password).then((result) => {
+//         if(result) {
+//            const token = jwt.sign(account.username, process.env.ACCESS_TOKEN_SECRET)
+//             res.send({ accessToken: token, username: account.username });
+//         }else {
+//             res.status(400);
+//             res.send("Inccorect Username or Password !");
+//             res.end();
+//           }
+//     })
+//   }else{
+//     res.status(404);
+//     res.send("User Does not Exist !");
+//     res.end();
+//   }
+// });
 
 
 mongoose.connect(dbUrl)
---
+++ servers/authServer/routes/accountRoute.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const Account = require("../models/Account")
+
+const router = express.Router();
+
+
+router.post("/", async (req, res) => {
+    bcrypt.hash(req.body.password, 10).then(async (hashedPassword) => {
+    const account = new Account({ username: req.body.username, password: hashedPassword });
+    try{
+        await account.save();
+        res.send(account)
+    }
+     catch(err) {
+    res.status(500);
+    res.send(err);
+  }
+});
+})
+
+
+router.post("/login", async (req, res) => {
+  const account = await Account.findOne({username: req.body.username});
+  if (account) {
+    bcrypt.compare(req.body.password, account.password).then((result) => {
+        if(result) {
+           const token = jwt.sign(account.username, process.env.ACCESS_TOKEN_SECRET)
+            res.send({ accessToken: token, username: account.username });
+        }else {
+            res.status(400);
+            res.send("Inccorect Username or Password !");
+            res.end();
+          }
+    })
+  }else{
+    res.status(404);
+    res.send("User Does not Exist !");
+    res.end();
+  }
+});
+
+
+module.exports = router;
